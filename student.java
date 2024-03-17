212ba521fc7f1396709056b9a4bc181c7ad9803d
vaishnavia <01fe22bca201@kletech.ac.in>
2024-03-17 20:33:12 +0530

    extended

--- student.java
+++ student.java
@@ -11,7 +11,7 @@ public class student{
         System.out.println("program:"+program);
         System.out.println("name:"+branch);;
         for(int i=0;i<course.length;i++){
-            System.out.println("course:"+i);
+            System.out.println("course:"+course[i]);
         }
 
     }
