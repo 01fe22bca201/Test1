03e3463666e79e8f121a582d778d6fe11c195025
vaishnavia <01fe22bca201@kletech.ac.in>
2024-03-17 20:16:23 +0530

    code for student display'

---
+++ student.java
@@ -0,0 +1,18 @@
+
+public class student{
+    public void stud_details(){
+        String name="hello";
+        String usn="201";
+        String program="bca";
+        String branch="department of compuuter application";
+        String[] course={"c==","ml","c#"};
+        System.out.println("name:"+name);
+        System.out.println("usn:"+usn);
+        System.out.println("program:"+program);
+        System.out.println("name:"+branch);;
+        for(int i=0;i<course.length;i++){
+            System.out.println("course:"+i);
+        }
+
+    }
+}
